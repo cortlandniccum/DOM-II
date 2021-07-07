3cbbb8e6eb382a83e359d32850599fd58b494e6c
cortlandniccum <cortlandniccum@gmail.com>
2021-07-06 20:07:57 -0600

    made the nav change color and size

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//Nav
+const nav = document.querySelector("nav");
+nav.addEventListener("mouseover", (event) => {
+  event.target.style.color = "purple";
+  event.target.style.fontSize = "x-large";
+});
+nav.addEventListener("mouseout", (event) => {
+  event.target.style.color = "black";
+  event.target.style.fontSize = "large";
+});
+
+//h2
+const h2 = document.querySelector("#logo-heading");
+h2.addEventListener("mouseover", (event) => {});
